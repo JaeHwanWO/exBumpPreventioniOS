834775ff918746eedcff8649f168efe3bcabc584
Yejin_Cho <yy991125@naver.com>
2019-11-20 22:25:57 +0900

    added .gitignore

---
+++ exBumpPreventioniOS/Pods/Target Support Files/Pods-exBumpPreventioniOS/Pods-exBumpPreventioniOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_exBumpPreventioniOS : NSObject
+@end
+@implementation PodsDummy_Pods_exBumpPreventioniOS
+@end
---
+++ exBumpPreventioniOS/exBumpPreventioniOS/ApiService.swift
@@ -0,0 +1,15 @@
+//
+//  ApiServuce.swift
+//  exBumpPreventioniOS
+//
+//  Created by 조예진 on 2019/11/20.
+//  Copyright © 2019 조예진. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ApiService {
+  static var baseHostName = "http://127.0.0.1/"
+}
