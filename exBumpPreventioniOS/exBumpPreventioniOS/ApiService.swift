99316ce87480d37a71154c278a118fd02a8502a7
Yejin_Cho <yy991125@naver.com>
2019-11-22 18:26:09 +0900

    앱 라우팅 코드 완성

--- exBumpPreventioniOS/exBumpPreventioniOS/ApiService.swift
+++ exBumpPreventioniOS/exBumpPreventioniOS/ApiService.swift
@@ -11,5 +11,65 @@ import Alamofire
 import SwiftyJSON
 
 class ApiService {
-  static var baseHostName = "http://127.0.0.1/"
+  static var baseHostName = "http://127.0.0.1:3000"
+  enum Router: URLRequestConvertible {
+    
+    case host(myUUID:String)
+    case invite(myUUID:String, randomCode:String)
+    case updateGPS(myUUID:String, latitude:Float, longitude:Float)
+    
+    // MARK:- URLRequestConvertible
+    func asURLRequest() throws -> URLRequest {
+      let result: (path: String, parameters: Parameters, method: HTTPMethod) = {
+        switch self {
+        case let .host(myUUID):
+          return ("/user/host", ["myUUID": myUUID], .post)
+        case let .invite(myUUID, randomCode):
+          return ("/user/invite", ["myUUID": myUUID, "randomCode": randomCode], .post)
+        case let .updateGPS(myUUID, latitude, longitude):
+          return ("/user/updateGPS", ["myUUID": myUUID, "latitude": latitude, "longitude": longitude], .post)
+        }
+      }()
+      let url = try (baseHostName + "/api").asURL()
+      let urlRequest = try URLRequest(url: url.appendingPathComponent(result.path), method: result.method)
+      print("my url is...")
+      print(url)
+      return try JSONEncoding.default.encode(urlRequest, with: result.parameters)
+    }
+  }
+  
+  static func host(myUUID: String, callback: ((String)->())?) {
+    Alamofire.request(Router.host(myUUID: myUUID)).responseJSON { (response) in
+      
+      guard let jsonData = response.result.value, response.result.isSuccess else {
+        callback?("error host")
+        return
+      }
+      let json = JSON(jsonData)
+      callback?(json["randomCode"].stringValue)
+    }
+  }
+  
+  static func invite(myUUID:String, randomCode:String) {
+    Alamofire.request(Router.invite(myUUID: myUUID, randomCode: randomCode)).responseJSON { (response) in
+      
+      guard let jsonData = response.result.value, response.result.isSuccess else {
+        return
+      }
+      let json = JSON(jsonData)
+      print(json)
+    }
+  }
+  
+  static func updateGPS(myUUID: String, latitude: Float, longitude: Float){
+    Alamofire.request(Router.updateGPS(myUUID: myUUID, latitude: latitude, longitude: longitude)).responseJSON { (response) in
+         
+         guard let jsonData = response.result.value, response.result.isSuccess else {
+           return
+         }
+         let json = JSON(jsonData)
+         print(json)
+       }
+  }
+  
 }
--- exBumpPreventioniOS/exBumpPreventioniOS/ViewController/MainViewController.swift
+++ exBumpPreventioniOS/exBumpPreventioniOS/ViewController/MainViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var testButton: UIButton!
+  override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  @IBAction func didTapTestButton(_ sender: Any) {
+    print("mainbutton call")
+    ApiService.updateGPS(myUUID: "yejinCho", latitude: 123.123, longitude: 124.124)
+  }
 
 }
